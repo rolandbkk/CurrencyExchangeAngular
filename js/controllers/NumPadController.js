872b12e1bc6cccbeb15018b6850236aa071fad83
rolandbkk <info@rw-webdesign.com>
2015-10-24 20:20:47 +0700

    Update NumPadController.js

--- js/controllers/NumPadController.js
+++ js/controllers/NumPadController.js
@@ -7,28 +7,6 @@ $scope.deleteLastNum = deleteLastNum;
 $scope.addDecimal = addDecimal;
 $scope.changeRate = changeRate;
 
-$scope.changeMode = changeMode;
-
-
-function changeMode(fromlab,tolab,fromicon,toicon,ratiolab) {
-  
-
-           $scope.from_label_output =fromlab;
-           $scope.to_label_output = tolab;
-           $scope.img_url_from = fromicon;
-           $scope.img_url_to = toicon;
-           $scope.ratio = ratiolab;
-           
-
-
-
-           $scope.to_num = parseInt($scope.from_num, 10)*$scope.ratio;
-
-
-}
-
-
-
 
 function changeRate() {
 
